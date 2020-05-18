ce115a56373ac744850bc98ec0219c0edb3f7bcf
Lishangwen <lswscu@foxmail.com>
2020-05-19 01:03:05 +0800

    测试密码

--- Code/Lishangwen/Cpp/Main.cpp
+++ Code/Lishangwen/Cpp/Main.cpp
@@ -5,6 +5,7 @@ int main(){
 	numPairsDivisibleBy60Test(lib);
 }
 
+
 void numPairsDivisibleBy60Test(LeetCodeLib lib){
 	vector<int> input{ 30,20,150,100,40 };
 	auto result = lib.numPairsDivisibleBy60(input);
