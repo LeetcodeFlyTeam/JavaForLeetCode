06aca9cab121126da81ed39498970714249b82f7
LiShangwen <lswscu@foxmail.com>
2020-05-18 23:47:32 +0800

    更新Cpp工程

---
+++ Code/Lishangwen/Cpp/LeetCodeLib.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "vector"
+using namespace std;
+class LeetCodeLib
+{
+public:
+	// 1010. 总持续时间可被 60 整除的歌曲
+	int numPairsDivisibleBy60(vector<int>& time);
+	bool checkSubarraySum(vector<int>& nums, int k);
+};
---
+++ Code/Lishangwen/Cpp/Main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Main.h"
+int main()
+{
+	LeetCodeLib lib = LeetCodeLib();
+	numPairsDivisibleBy60Test(lib);
+}
+
+void numPairsDivisibleBy60Test(LeetCodeLib lib)
+{
+	vector<int> input{ 30,20,150,100,40 };
+	auto result = lib.numPairsDivisibleBy60(input);
+	cout << result << endl;
+}
+
+void checkSubarraySumTest(vector<int>& nums, int k)
+{
+
+}
