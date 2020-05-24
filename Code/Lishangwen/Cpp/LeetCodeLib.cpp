967d984d7df5c20f69afd91cd041d885d505c583
Lishangwen <lswscu@foxmail.com>
2020-05-24 18:38:59 +0800

    简化代码

--- Code/Lishangwen/Cpp/LeetCodeLib.cpp
+++ Code/Lishangwen/Cpp/LeetCodeLib.cpp
@@ -51,12 +51,7 @@ int LeetCodeLib::uniquePaths(int m, int n) {
 	vector<vector<int>> pathCache(m,vector<int>(n,0));
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++){
-			if (i == 0 || j == 0) {
-				pathCache[i][j] = 1;
-			}
-			else{
-				pathCache[i][j] = pathCache[i][j - 1] + pathCache[i - 1][j];
-			}
+			pathCache[i][j] = (i == 0 || j == 0)? 1: pathCache[i][j - 1] + pathCache[i - 1][j];
 		}
 	}
 	return pathCache[m - 1][n - 1];
